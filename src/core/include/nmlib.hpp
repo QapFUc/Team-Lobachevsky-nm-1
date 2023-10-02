6b6632b75d4f0dd1ce2a96ef3dd2b66cfde8650c
Vladislav Suchkov <v.suchkov@yadro.com>
2023-10-02 21:42:22 +0300

    some arch and functions

--- src/core/include/nmlib.hpp
+++ src/core/include/nmlib.hpp
@@ -1,15 +1,59 @@
 #pragma once
+#include <vector>
+
+typedef unsigned long uint;
+
+struct tableRow {
+// one row of table for nm output (see actual task)
+    float xi;
+    float vi;
+    float v2i;
+    float viv2i;
+    float localError;
+    uint hi;
+    uint C1;
+    uint C2;
+    float ui;
+    float uvi;
+};
+
+struct resultTable {
+// output table (see actual task)
+    std::vector<tableRow> rows;
+};
+struct config {   
+    // Left and right limits for x variable
+    float x_min; 
+    float x_max;
+
+    // initial point
+    float x_0; 
+    float u_0;
+
+    float step; // step
+    uint N_max; // Maximum num for iterations
+
+    float eps = 0.f; // Epsilon for local error control
+};
 
 /// @brief Namespace utils is needed to provide utilities that will be used in core functions
 namespace utils {
-/// @todo make some utils
+
+/// @brief make config from python input
+config make_config(float x_min, float x_max, float x_0, float u_0, float step, uint N_max, float eps);
+
+/// @brief make output table for python
+/// @todo implementation for this func
+void make_out_from_table(resultTable result);
 } // namespace utils
 
-/// @brief This functions only for test
-/// @todo Real func declarations
+/// @brief  This is the core functions block. Each of them calculate one of examples from main task (see actual task) 
+/// @todo write an implementation for each one
+resultTable test_task_a(config cfg);
+resultTable test_task_b(config cfg);
 
-void test_task(int N, float *xs, float *ys, float *step);
+resultTable first_task_a(config cfg);
+resultTable first_task_b(config cfg);
 
-void first_task(int N, float *xs, float *ys, float *step, float eps);
-
-void second_task(int N, float *xs, float *ys, float *step, float eps);
+resultTable second_task_a(config cfg);
+resultTable second_task_b(config cfg);
--- src/core/src/nmlib.cc
+++ src/core/src/nmlib.cc
@@ -1,6 +1,6 @@
-#include <iostream>
 #include "nmlib.hpp"
 
-void test_task() {
-    return;
+config utils::make_config(float x_min, float x_max, float x_0, float u_0, float step, uint N_max, float eps) {
+    config cfg = {.x_min = x_min, .x_max = x_max, .x_0 = x_0, .u_0 = u_0, .step = step, .N_max = N_max, .eps = eps};
+    return cfg;
 }
