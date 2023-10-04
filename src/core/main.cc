26a54d6f226021ee630bef790c2f18d6ee7fd643
Vladislav Suchkov <v.suchkov@yadro.com>
2023-10-04 18:57:41 +0300

    logger and stuff

--- src/core/main.cc
+++ src/core/main.cc
@@ -1,7 +1,5 @@
 #include "include/nmlib.hpp"
 
 int main() {
-    LOG_INFO_CLI("startup");
-    NM_ASSERT((1+1==3), "error");
     return 0;
 }
--- src/core/src/nmlib.cc
+++ src/core/src/nmlib.cc
@@ -6,8 +6,10 @@ config utils::make_config(float x_min, float x_max, float x_0, float u_0, float 
     LOG_DEBUG_CLI("Making config from python data", "Checking arguments");
 #endif
     NM_ASSERT((x_max >= x_min), "invalid argument x_min > x_max");
+    /// @todo write an asserts for other args
 
     config cfg {x_min, x_max, x_0, u_0, step, N_max, eps};
+    LOG_INFO_CLI("Configuration done");
     return cfg;
 }
 
@@ -31,19 +33,35 @@ extern "C" tableRow* run_from_python(char *func_name,
                                      uint N_max,
                                      float eps,
                                      uint *rowsCount) {
+    LOG_INFO_CLI("Function is running");
+#ifdef DEBUG
+    LOG_DEBUG_CLI("params for config", std::string(func_name), x_min, x_max, x_0, u_0, step, N_max, eps, rowsCount);
+#endif
     resultTable result = utils::functions[func_name](utils::make_config(x_min, x_max, x_0, u_0, step, N_max, eps));
 
     *rowsCount = result.size();
-    
+    LOG_INFO_CLI("Function run succesfuly");
     return result.data();
 }
 
 /// @todo Implement core functions
-resultTable test_task_a(config cfg) {}
-resultTable test_task_b(config cfg) {}
+resultTable test_task_a(config cfg) {
+    [](){};
+}
+resultTable test_task_b(config cfg) {
+    [](){};
+}
 
-resultTable first_task_a(config cfg) {}
-resultTable first_task_b(config cfg) {}
+resultTable first_task_a(config cfg) {
+    [](){};
+}
+resultTable first_task_b(config cfg) {
+    [](){};
+}
 
-resultTable second_task_a(config cfg) {}
-resultTable second_task_b(config cfg) {}
+resultTable second_task_a(config cfg) {
+    [](){};
+}
+resultTable second_task_b(config cfg) {
+    [](){};
+}
