c099e4549b31e64ce96f167710600d3fef2f78d4
qapfuc <qapfuc@gmail.com>
2023-10-05 15:37:57 +0300

    Start RK4. And construct for table row

--- src/core/include/nmlib.hpp
+++ src/core/include/nmlib.hpp
@@ -8,12 +8,15 @@
 struct tableRow {
 /// @todo write the constructor for init
 // one row of table for nm output (see actual task)
+    tableRow(float xi, float vi, float v2i, float viv2i,
+            float LE, float hi, uint C1, uint C2, float ui,
+            float uvi) : xi(xi), vi(vi), v2i(v2i), viv2i(viv2i), LE(LE), hi(hi), C1(C1), C2(C2), ui(ui), uvi(uvi);
     float xi;
     float vi;
     float v2i;
     float viv2i;
     float LE;
-    uint hi;
+    float hi;
     uint C1;
     uint C2;
     float ui;
@@ -35,6 +38,7 @@ struct config {
     float step; // step
     uint N_max; // Maximum num for iterations
 
+    bool LEC = 1; // Is there control for local error
     float eps = 0.f; // Epsilon for local error control
 };
 
@@ -54,6 +58,9 @@ resultTable second_task_b(config cfg);
 namespace utils {
 /// @brief make config from python input
 config make_config(float x_min, float x_max, float x_0, float u_0, float step, uint N_max, float eps);
+
+/// numerical method 
+resultTable RK4(std::function<float(float,float)> rhs, config cfg);
 } // namespace utils
 
 /// @brief  Function that will be called from python. 
--- src/core/src/methods.cc
+++ src/core/src/methods.cc
@@ -1,5 +1,22 @@
 #include "nmlib.hpp"
 
+resultTable utils::RK4(std::function<float(float,float)> rhs, config cfg) {
+    resultTable table;
+    float xi = cfg.x_min;
+    float vi = cfg.u_0;
+    float stepi = cfg.step;
+    i = 0
+    while (xi <= cfg.x_max && i <= cfg.N_max) {
+        tableRow row;
+        k1 =  rhs(xi ,vi);
+        k2 = rhs(xi+stepi/2 ,vi + (stepi/2) * k1);
+        k3 = rhs(xi+stepi/2 ,vi + (stepi/2) * k2);
+        k4 = rhs(xi+stepi ,vi + stepi * k3);
+        xi = xi + stepi;
+        vi = vi + step/6 +( k1 + 2 * k2 + 2 * k3 + k4 );
+        
+    }
+}
 
 /// @todo Implement core functions
 resultTable test_task_a(config cfg) {
