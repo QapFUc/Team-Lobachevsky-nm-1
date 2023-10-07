4e112c39bbf37d3aa87a29b7ace0cfb37e84cff6
Dmitriy Usov <69606738+QapFUc@users.noreply.github.com>
2023-10-07 21:20:04 +0300

    Update nmlib.cc

--- src/core/src/nmlib.cc
+++ src/core/src/nmlib.cc
@@ -1,45 +1,105 @@
 #include "nmlib.hpp"
+#include <cmath>
 
-config utils::make_config(float x_min, float x_max, float x_0, float u_0, float step, uint N_max, float eps) {
-    LOG_INFO_CLI("Making some congig");
-#if defined(DEBUG)
-    LOG_DEBUG_CLI("Making config from python data", "Checking arguments");
-#endif
-    NM_ASSERT((x_max >= x_min), "invalid argument x_min > x_max");
-    /// @todo write an asserts for other args
+float utils::StepRK4(std::function<float(float,float)> rhs, float x, float u, float step) {
 
-    config cfg {x_min, x_max, x_0, u_0, step, N_max, eps};
-    LOG_INFO_CLI("Configuration done");
-    return cfg;
+    //LOG_INFO_CLI("Start RK4 step with following config", x, u, step);
+
+    float new_u, k1, k2, k3, k4;
+    k1 = rhs(x, u);
+    k2 = rhs(x + step/2, u + step/2 * k1);
+    k3 = rhs(x + step/2, u + step/2 * k2);
+    k4 = rhs(x + step, u + step * k3);
+    new_u = u + step/6 * (k1 + (2 * k2) + (2 * k3) + k4);
+    return(new_u);
 }
 
-namespace utils {
-std::map<char*, std::function<resultTable(config)>> functions = {
-    {"TEST_TASK_A", test_task_a},
-    {"TEST_TASK_B", test_task_b},
-    {"FIRST_TASK_A", first_task_a},
-    {"FIRST_TASK_B", first_task_b},
-    {"SECOND_TASK_A", second_task_a},
-    {"SECOND_TASK_B", second_task_b}
-};
+resultTable utils::RK4(std::function<float(float,float)> rhs, config cfg) {
+    LOG_INFO_CLI("Start RK4 with following config", cfg);
+    
+    resultTable table;
+    float xi, x_min, x_max, ui, stepi, N_max, eps;
+    uint C1, C2;
+    C1 = 0;
+    C2 = 0;
+    xi = cfg.x_0;
+    x_min = cfg.x_min;
+    x_max = cfg.x_max;
+    ui = cfg.u_0;
+    stepi = cfg.step;
+    N_max = cfg.N_max;
+    eps = cfg.eps;
+
+    if (cfg.LEC) {
+        int i = 0;
+        float u1, u2;
+        float viv2i, LocalError;
+        while (xi >= x_min && xi <= x_max && i < N_max) {
+            u1 = StepRK4(rhs, xi, ui, stepi);
+            u2 = StepRK4(rhs, xi, ui, stepi/2);
+            u2 = StepRK4(rhs, xi + stepi/2, u2, stepi/2);
+
+            LocalError = (std::abs(u1 - u2))/(std::pow(2,4) - 1);
+            if (LocalError < eps/std::pow(2,5)) {
+                ui = u1;
+                xi = xi + stepi;
+                viv2i = std::abs(ui - u2);
+                tableRow row(xi, ui, u2, viv2i, LocalError, stepi, C1, C2, 0.f, 0.f);
+                table.push_back(row);
+                stepi = stepi * 2;
+                C2++;
+                i++;
+            } else if (LocalError >= eps/std::pow(2,5) && LocalError <= eps ) {
+                ui = u1;
+                xi = xi + stepi;
+                viv2i = std::abs(ui - u2);
+                tableRow row(xi, ui, u2, viv2i, LocalError, stepi, C1, C2, 0.f, 0.f);
+                table.push_back(row);
+                i++;
+                
+            } else if (LocalError > eps) {
+                stepi = stepi / 2;
+                C1++;
+                i++;
+            } else {
+                LOG_INFO_CLI("Error in RK4 with LEC", cfg);
+            }
+        }
+        return(table);
+
+    } else if (not(cfg.LEC)){
+        int i = 0;
+        while (xi >= x_min && xi <= x_max && i < N_max) {
+            ui = StepRK4(rhs, xi, ui, stepi);
+            xi = xi + stepi;
+            i++;
+            tableRow row(xi, ui, 0.f, 0.f, 0.f, stepi, 0.f, 0.f, 0.f, 0.f);
+            table.push_back(row);
+        }
+        return(table);
+    } else {
+        LOG_INFO_CLI("Error in RK4", cfg);
+    }
 }
 
-extern "C" tableRow* run_from_python(char *func_name,    
-                                     float x_min, 
-                                     float x_max,
-                                     float x_0, 
-                                     float u_0,
-                                     float step,
-                                     uint N_max,
-                                     float eps,
-                                     uint *rowsCount) {
-    LOG_INFO_CLI("Function is running");
-#if defined(DEBUG)
-    LOG_DEBUG_CLI("params for config", std::string(func_name), x_min, x_max, x_0, u_0, step, N_max, eps, rowsCount);
-#endif
-    resultTable result = utils::functions[func_name](utils::make_config(x_min, x_max, x_0, u_0, step, N_max, eps));
+/// @todo Implement core functions
+resultTable test_task_a(config cfg) {
+    [](){};
+}
+resultTable test_task_b(config cfg) {
+    [](){};
+}
 
-    *rowsCount = result.size();
-    LOG_INFO_CLI("Function run succesfuly");
-    return result.data();
+resultTable first_task_a(config cfg) {
+    [](){};
+}
+resultTable first_task_b(config cfg) {
+    [](){};
+}
+
+resultTable second_task_a(config cfg) {
+    [](){};
+}
+resultTable second_task_b(config cfg) {
+    [](){};
 }
