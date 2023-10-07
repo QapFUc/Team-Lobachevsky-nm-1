4cc88756a8a47ccba604458dbfed3affd671fd81
qapfuc <qapfuc@gmail.com>
2023-10-07 17:06:47 +0300

    fixed? 2.0

--- src/core/main.cc
+++ src/core/main.cc
@@ -5,13 +5,13 @@ float const_rhs1(float x, float v) {
 }
 
 float const_rhs2(float x, float v) {
-        return 2.f*x;
+        return 2.f*x*x/7+x/3;
 }
 
 int main() {
    resultTable res1 = utils::RK4(const_rhs1, utils::make_config(0.f, 5.f, 0.f, 0.f, 0.05, 1000, false,0.0));
    LOG_DEBUG_CLI("Result is ", res1.at(2));
-   resultTable res2 = utils::RK4(const_rhs2, utils::make_config(0.f, 5.f, 0.f, 0.f, 0.05, 1000, false,0.0));
+   resultTable res2 = utils::RK4(const_rhs2, utils::make_config(0.f, 5.f, 0.f, 0.f, 0.5, 1000, false,0.0000001));
    LOG_DEBUG_CLI("Result is ", res2.at(2));
    return 0;
 }
