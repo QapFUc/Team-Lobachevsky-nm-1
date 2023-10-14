f0f6a73c1cd81b10691e8c1418f0c2932fad32b6
qapfuc <qapfuc@gmail.com>
2023-10-14 20:35:08 +0000

    add support for mac

--- main.cc
+++ main.cc
@@ -5,7 +5,7 @@ float test_rhs(float x, float v) {
 }
 
 int main() {
-   resultTable res1 = utils::RK4(test_rhs, utils::make_config(0.f, 1e15, 0.f, 1.f, 0.f, 0.01, 1e15, true, 1e-6));
+   resultTable res1 = utils::RK4(test_rhs, utils::make_config(0.f, 40, 0.f, 1.f, 0.f, 0.01, 10000, true, 1e-6));
    for(int i = 0 ; i < res1.size(); i++) {
         LOG_DEBUG_CLI("Result is ", res1.at(i));
    }
