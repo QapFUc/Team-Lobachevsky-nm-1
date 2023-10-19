0d48c80a8dc849641c4234343c7c09c996dc1bfc
qapfuc <qapfuc@gmail.com>
2023-10-19 19:33:17 +0000

    checkme

--- src/core/src/methods.cc
+++ src/core/src/methods.cc
@@ -86,7 +86,7 @@ resultTable utils::RK4(std::function<float(float,float)> rhs, const config& cfg)
 
 inline std::vector<float> utils::StepRK4_SOE(std::function<float(float,float,float)> rhs1,std::function<float(float,float,float)> rhs2, const float& x, const float& u, const float& y, const float& step) {
 
-    //LOG_INFO_CLI("Start RK4 step with following config", x, u, step);
+    LOG_DEBUG_CLI("Start StepRK4_SOE step with following config", x, u, step);
 
     float new_u, new_y, k11, k12, k21, k22, k31, k32, k41, k42;
     std::vector<float> res;
@@ -106,6 +106,7 @@ inline std::vector<float> utils::StepRK4_SOE(std::function<float(float,float,flo
 }
 
 resultTable utils::RK4_SOE(std::function<float(float, float, float)> rhs1, std::function<float(float, float, float)> rhs2, const config& cfg) {
+    LOG_INFO_CLI("Start RK4_SOE with following config", cfg);
     resultTable table;
     float xi, x_min, x_max, ui, yi, stepi, N_max, eps;
     uint C1, C2;
@@ -123,38 +124,48 @@ resultTable utils::RK4_SOE(std::function<float(float, float, float)> rhs1, std::
     if (cfg.LEC) {
         float LocalError, viv2i;
         int i = 0;
-        tmp1 = utils::StepRK4_SOE(rhs1, rhs2, xi, ui, yi, stepi);
-        tmp2 = utils::StepRK4_SOE(rhs1, rhs2, xi, ui, yi, stepi/2);
-        tmp2 = utils::StepRK4_SOE(rhs1, rhs2, xi, tmp2.at(0), tmp2.at(1), stepi/2);
-        LocalError = std::max(std::abs(tmp1.at(0) - tmp2.at(0)), std::abs(tmp1.at(1)-tmp2.at(1))) / 15;
-        if (LocalError < eps/std::pow(2,5)) {
-            yi = tmp1.at(1);
-            ui = tmp1.at(0);
-            xi = xi + stepi;
-            viv2i = std::abs(ui - tmp2.at(0));
-            tableRow row(xi, ui, yi, tmp1.at(0), tmp2.at(1), viv2i, LocalError, stepi, C1, C2, 0.f, 0.f);
-            table.push_back(row);
-            stepi = stepi * 2;
-            C2++;
-            i++;
-        } else if (LocalError >= eps/std::pow(2,5) && LocalError <= eps ) {
-            yi = tmp1.at(1);
-            ui = tmp1.at(0);
-            xi = xi + stepi;
-            viv2i = std::abs(ui - tmp2.at(0));
-            tableRow row(xi, ui, yi, tmp2.at(0), tmp2.at(1), viv2i, LocalError, stepi, C1, C2, 0.f, 0.f);
-            table.push_back(row);
-            i++; 
-        } else if (LocalError > eps) {
-            stepi = stepi / 2;
-            C1++;
-            i++;
-        } else {
-            LOG_ERROR_CLI(cfg);
+        while (xi >= x_min && xi <= x_max && i < N_max) {
+            LOG_DEBUG_CLI("Start RK4_SOE with localstecpcontrol", cfg);
+            tmp1 = utils::StepRK4_SOE(rhs1, rhs2, xi, ui, yi, stepi);
+            tmp2 = utils::StepRK4_SOE(rhs1, rhs2, xi, ui, yi, stepi/2);
+            tmp2 = utils::StepRK4_SOE(rhs1, rhs2, xi, tmp2.at(0), tmp2.at(1), stepi/2);
+            LocalError = std::max(std::abs(tmp1.at(0) - tmp2.at(0)), std::abs(tmp1.at(1)-tmp2.at(1))) / 15;
+            LOG_DEBUG_CLI("tmp generated", cfg);
+            if (LocalError < eps/std::pow(2,5)) {
+                LOG_DEBUG_CLI("try LocalErroe < lec", cfg);
+                yi = tmp1.at(1);
+                ui = tmp1.at(0);
+                xi = xi + stepi;
+                viv2i = std::abs(ui - tmp2.at(0));
+                LOG_DEBUG_CLI("viv2i generated", cfg);
+                tableRow row(xi, ui, yi, tmp1.at(0), tmp2.at(1), viv2i, LocalError, stepi, C1, C2, 0.f, 0.f);
+                LOG_DEBUG_CLI("try push_back", cfg);
+                table.push_back(row);
+                LOG_DEBUG_CLI("push_back done", cfg);
+                stepi = stepi * 2;
+                C2++;
+                i++;
+                LOG_DEBUG_CLI("LocalErroe < lec done", cfg);
+            } else if (LocalError >= eps/std::pow(2,5) && LocalError <= eps ) {
+                LOG_DEBUG_CLI("LocalErroe >= lec", cfg);
+                yi = tmp1.at(1);
+                ui = tmp1.at(0);
+                xi = xi + stepi;
+                viv2i = std::abs(ui - tmp2.at(0));
+                tableRow row(xi, ui, yi, tmp2.at(0), tmp2.at(1), viv2i, LocalError, stepi, C1, C2, 0.f, 0.f);
+                table.push_back(row);
+                i++; 
+            } else if (LocalError > eps) {
+                LOG_DEBUG_CLI("LocalErroe > lec*", cfg);
+                stepi = stepi / 2;
+                C1++;
+                i++;
+            } else {
+                LOG_ERROR_CLI(cfg);
+            }
         }
-
-
     } else if (not(cfg.LEC)){
+        LOG_DEBUG_CLI("Start RK4_SOE without localstecpcontrol", cfg);
         int i = 0;
         while (xi >= x_min && xi <= x_max && i < N_max) {
             tmp1 = utils::StepRK4_SOE(rhs1, rhs2, xi, ui, yi, stepi);
